15635181889079498247459f48606dfe51636eb8
Nemesix493 <verquerre.kevin@gmail.com>
2024-06-18 21:51:10 +0200

    feat(managers): add method and update method
    add save method
    update get_path to staticmethod

--- vocab/managers/base.py
+++ vocab/managers/base.py
@@ -1,9 +1,24 @@
-import abc
+from abc import (
+    ABC,
+    abstractmethod
+)
+from json import (
+    dump as json_dump,
+    load as json_load
+)
 from pathlib import Path
 
 
-class BaseManager(abc.ABC):
-    @abc.abstractmethod
-    @classmethod
-    def get_path(cls, managed_obj) -> Path:
+class BaseManager(ABC):
+    @abstractmethod
+    @staticmethod
+    def get_path(managed_obj) -> Path:
         pass
+
+    @classmethod
+    def save(cls, managed_obj) -> None:
+        with open(cls.get_path(managed_obj), 'w', encoding='utf-8') as save_file:
+            json_dump(
+                managed_obj,
+                save_file
+            )
--- vocab/managers/lang_dict_manager.py
+++ vocab/managers/lang_dict_manager.py
@@ -5,6 +5,6 @@ BASE_LANG_DICT_DIR = Path(__file__).resolve().parent.parent.parent / 'dict'
 
 
 class LangDictManager(BaseManager):
-    @classmethod
-    def get_path(cls, managed_obj) -> Path:
+    @staticmethod
+    def get_path(managed_obj) -> Path:
         return BASE_LANG_DICT_DIR / f'{managed_obj.lang}.json'
